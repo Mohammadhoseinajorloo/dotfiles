a636f0b620cfbe756cfa2dc1869e97eb88e4c924
mohammad <mohammadhoseinajorloo76@gmail.com>
2026-08-18 17:53:01 +0330

    feat(awesome): add firfox music rule in rules.lua file

--- common/.config/awesome/config/rules.lua
+++ common/.config/awesome/config/rules.lua
@@ -1,6 +1,16 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 
+
+local function get_screen_by_name(name)
+  for s in screen do
+    if s.outputs and s.outputs[name] then
+      return s
+    end
+  end
+  return awful.screen.focused() or screen.primary or 1
+end
+
 return function(keys)
   awful.rules.rules = {
     {
@@ -51,6 +61,20 @@ return function(keys)
     {
       rule_any = { type = { "normal", "dialog" } },
       properties = { titlebars_enabled = true },
+    },
+    {
+      rule = { class = "songsara" },
+      properties = {
+        screen = function()
+          return get_screen_by_name("HDMI-1")
+        end,
+        tag = function()
+          local target_screen = get_screen_by_name("HDMI-1")
+          return target_screen.tags[9] or target_screen.tags[1]
+        end,
+        switchtotag = true,
+        ignore_tags = true,
+      }
     },
   }
 end
